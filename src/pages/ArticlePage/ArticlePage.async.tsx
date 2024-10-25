11b6040f21fb343fcd1b03c1e8ddb5887363d83e
Egor Danilov <egordanilov970@gmail.com>
2024-10-25 22:39:49 +0300

    removed set timeout

--- src/pages/ArticlePage/ArticlePage.async.tsx
+++ src/pages/ArticlePage/ArticlePage.async.tsx
@@ -1,8 +1,4 @@
 import {lazy} from "react";
 
-export const ArticlePageAsync = lazy(() => new Promise(resolve => {
-    // @ts-ignore
-    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
-    setTimeout(() => resolve(import('./ArticlePage')), 1500)
-}))
+export const ArticlePageAsync = lazy(() => import("./ArticlePage"));
 
